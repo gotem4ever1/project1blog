4735db0abade9ddb6b7eafc69c2f1c13c50a4d20
William Chin <gotem4ever1@gmail.com>
2021-01-27 20:45:52 +1100

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  #user validation
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  #email vqalidation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
+  format: { with: VALID_EMAIL_REGEX }
+  before_save { self.email = email.downcase }
 end
