a264a08aebe6cf7557af3dffae4a6e46dc49cd78
William Chin <gotem4ever1@gmail.com>
2021-01-28 23:33:47 +1100

    add association between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
 has_many :comments, dependent: :destroy  #comment the article and delete the comment
   validates :title, presence: true, #title for every articles
                     length: { minimum: 1 }
-
+validates :user_id, presence: true
 end
