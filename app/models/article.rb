f4e5c0cd0a43cd59398192c45b6d9a9e0404f631
William Chin <gotem4ever1@gmail.com>
2021-01-31 22:23:01 +1100

    Your commit message.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
 
 has_many :comments, dependent: :destroy
   #validates :title, presence: true, #error msgs
-                    length: { minimum: 1 }
+                    #length: { minimum: 1 }
 
 end
