0937fdf5463f957d3eca0ef0f2b871c29e944697
William Chin <gotem4ever1@gmail.com>
2021-01-31 22:14:11 +1100

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
 
-has_many :comments, dependent: :destroy 
-  validates :title, presence: true, #error msgs
+has_many :comments, dependent: :destroy
+  #validates :title, presence: true, #error msgs
                     length: { minimum: 1 }
 
 end
